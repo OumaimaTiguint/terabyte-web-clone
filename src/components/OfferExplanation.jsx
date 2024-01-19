b306e8eb320a146d44cd57b731138f99700c39c7
OumaimaTiguint <oumaimatiguint@gmail.com>
2024-01-19 15:40:24 +0100

    added count

--- src/components/OfferExplanation.jsx
+++ src/components/OfferExplanation.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import OfferExpCard from './OfferExpCard';
-import { offerExplanationData } from '../data/offerExplanationData'
+import { offerExplanationData } from '../data/offerExplanationData';
+import { countingLpcData } from '../data/countingLpcData';
 
 function OfferExplanation() {
     return (
@@ -17,6 +18,16 @@ function OfferExplanation() {
                                       title={offer.title}
                                       logo={offer.logo}
                                       description={offer.description}/>
+                    ))}
+                </div>
+
+                <div className='relative max-w-[67.3rem] flex flex-wrap m-auto justify-center items-center gap-5 mt-20 mb-36 element-to-fade-to-top element-to-start-counting fade-in-top'>
+                    {countingLpcData.map((e) => (
+                        <div key={e.id} className='h-16 justify-center items-center gap-1 flex text-white'>
+                            <h3 className='text-right sm:text-[50px] xs:text-4xl text-3xl font-semibold'>{e.number}</h3>
+                            <div className='w-5 h-5 bg-blue-200'></div>
+                            <p className='sm:text-xl xs:text-lg text-base font-normal'>{e.title}</p>
+                        </div>
                     ))}
                 </div>
             </div>
---
+++ src/data/countingLpcData.js
@@ -0,0 +1,17 @@
+export const countingLpcData = [
+    {
+        id: 0,
+        number: '2',
+        title: 'Our Location'
+    },
+    {
+        id: 1,
+        number: '+20',
+        title: 'Satisfied Clients'
+    },
+    {
+        id: 2,
+        number: '+2500',
+        title: 'Delivered Days'
+    }
+];
