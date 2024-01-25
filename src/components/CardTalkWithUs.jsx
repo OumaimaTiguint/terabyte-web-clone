7b81b5b61ed27cb5e479561f4fdc2e54f11e7af6
OumaimaTiguint <oumaimatiguint@gmail.com>
2024-01-25 10:28:28 +0100

    added talk to us bg and adjusted the padding

--- src/components/CardTalkWithUs.jsx
+++ src/components/CardTalkWithUs.jsx
@@ -2,21 +2,33 @@ import React from 'react';
 
 const CardTalkWithUs = () => {
     return (
-        <div className=' sm:px-16 px-4 w-full flex justify-center p-3 text-white'>
-            <div className='max-w-screen-2xl w-full p-5 pt-20 pb-28 mt-48 bg-tera-container-to-tr rounded-3xl border border-white border-opacity-10 flex flex-col justify-center items-center element-to-fade-to-top fade-in-top'>
-                <h2 className='text-center sm:text-[43px] xs:text-4xl text-3xl font-extrabold leading-[50px]'>
-                    Talk with us Freely without obligation
-                </h2>
-                <p className='max-w-[1004px] text-center sm:text-2xl xs:text-xl text-lg font-normal leading-9 pt-14'>
-                    We are at your service to discuss, analyze, propose, decipher, draw the roadmap or just advise you. Lets talk even if you are just curious. We have a saying that goes: Even if you're not buying you benefit.
-                </p>
+        <div className='relative pt-8 flex flex-col items-center'>
+            <div className='w-full flex justify-center p-3 text-white'>
+                <div className='max-w-screen-2xl w-full p-5 pt-20 pb-28 mt-48 bg-tera-container-to-tr rounded-3xl border border-white border-opacity-10 flex flex-col justify-center items-center element-to-fade-to-top fade-in-top z-1'>
+                    <h2 className='text-center sm:text-[43px] xs:text-4xl text-3xl font-extrabold leading-[50px]'>
+                        Talk with us Freely without obligation
+                    </h2>
+                    <p className='max-w-[1004px] text-center sm:text-2xl xs:text-xl text-lg font-normal leading-9 pt-14'>
+                        We are at your service to discuss, analyze, propose, decipher, draw the roadmap or just advise you. Lets talk even if you are just curious. We have a saying that goes: Even if you're not buying you benefit.
+                    </p>
 
-                <div className="mt-12 flex justify-center">
-                    <button className='bg-[#325588] text-[#b3c0d3] rounded-full shadow-lg py-2 px-10 transition-transform transform hover:scale-105 hover:opacity-80 hover:border hover:border-white hover:text-white'>
-                        Let's Talk
-                    </button>
+                    <div className="relative w-full flex justify-center mt-9 z-10">
+                        <a href="#contact" 
+                           class="absolute py-3 px-10 bg-white bg-opacity-20 rounded-3xl flex items-center justify-center  text-sm font-semibold shining-btn">
+                            Let's Talk
+                        </a>
+                    </div>
                 </div>
             </div>
+            {/* Gradient on the left */}
+            <div className='absolute left-0 scale-x-125'>
+                <img src="https://terabyte-web.com/img/background_svg/gradient_1.svg"
+                     width="806"
+                     height="1687"
+                     alt='gradient' />
+            </div>
+            {/* Gradient on the bottom */}
+            <div className='w-full h-[16.7rem] absolute bottom-0 bg-gradient-to-t from-[#0B2359] to-[#0B2359] z-[-1]'></div>
         </div>
     );
 };
